44768fa3d4d3a6a44bbf15bb4c1d70656a26ec2c
TinaxLee <168499038+TinaxLee@users.noreply.github.com>
2026-03-20 20:13:55 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 每次更新這個數字就好，用時間戳記確保唯一
-const BUILD = '1773998474';
+const BUILD = '1748000000';
 const CACHE = `yallabi-${BUILD}`;
 const ASSETS = ['./', './index.html', './style.css', './manifest.json', './icon.png'];
 
@@ -16,37 +16,11 @@ self.addEventListener('activate', e => {
   );
 });
 
+// Network-first：優先用網路，失敗才用快取
 self.addEventListener('fetch', e => {
   e.respondWith(
     fetch(e.request)
       .then(res => {
-        const resClone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, resClone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
-});
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', e => {
-  // Network-first：優先用網路，失敗才用快取
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        // 更新快取
         const resClone = res.clone();
         caches.open(CACHE).then(c => c.put(e.request, resClone));
         return res;
