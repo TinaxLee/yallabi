1fc95ee0c244f1007de7e279a67eb9add5ad696c
TinaxLee <168499038+TinaxLee@users.noreply.github.com>
2026-03-20 21:19:52 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,30 +1,6 @@
-// 每次更新這個數字就好，用時間戳記確保唯一
-const BUILD = '1748100000';
+const BUILD = '1774012739';
 const CACHE = `yallabi-${BUILD}`;
 const ASSETS = ['./', './index.html', './style.css', './manifest.json', './icon.png'];
-
-self.addEventListener('install', e => {
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
-});
-
-// Network-first：優先用網路，失敗才用快取
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const resClone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, resClone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
-});
+self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
+self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
+self.addEventListener('fetch', e => { e.respondWith(fetch(e.request).then(res => { caches.open(CACHE).then(c => c.put(e.request, res.clone())); return res; }).catch(() => caches.match(e.request))); });
