05f9e582d64880a45322913df2cc3e9b04ac46fa
TinaxLee <168499038+TinaxLee@users.noreply.github.com>
2026-03-20 13:18:08 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // 每次更新這個數字就好，用時間戳記確保唯一
-const BUILD = '1773983602';
+const BUILD = '1773983838';
 const CACHE = `yallabi-${BUILD}`;
 const ASSETS = ['./', './index.html', './style.css', './manifest.json', './icon.png'];
 
