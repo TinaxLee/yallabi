10d1e4c332beb924921b2f43690a8d2dc9912c37
TinaxLee <168499038+TinaxLee@users.noreply.github.com>
2026-03-21 18:08:07 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const BUILD = '1774021945';
+const BUILD = '1774085745';
 const CACHE = `yallabi-${BUILD}`;
 const ASSETS = ['./', './index.html', './style.css', './manifest.json', './icon.png'];
 self.addEventListener('install', e => { self.skipWaiting(); e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
